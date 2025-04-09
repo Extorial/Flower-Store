762e94ed571b301227bf322a4a137cb7456c503c
Extorial <christopherwang197@gmail.com>
2025-04-09 15:38:27 -0400

    Create base template and apply to home page
    Create base template, download background image, and apply base + image to home page. Django book chapter 3.

--- flowerstore/home/views.py
+++ flowerstore/home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {} #to pass data from views to template
+    template_data['title'] = 'Flower Store'
+    return render(request, 'home/index.html', {
+        'template_data': template_data
+    })
 def about(request):
     return render(request, 'home/about.html')   
