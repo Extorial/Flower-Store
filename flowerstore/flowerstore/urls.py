162601f788cee7fcdae0cc9bd1aa85cbd7ab1970
Extorial <christopherwang197@gmail.com>
2025-04-16 23:29:53 -0400

    Add Model to admin
    Add Flower model to admin page, and now able to add flowers on the admin page with description, price, name, and image. Add image upload ability with Pillow. Finished Chapter 5 of Django Book

--- flowerstore/flowerstore/urls.py
+++ flowerstore/flowerstore/urls.py
@@ -19,9 +19,13 @@ Including another URLconf
 #app-level URL file passes the interaction to a views.py file
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('flowers/', include("flowers.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
+
