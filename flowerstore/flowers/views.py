0696b0cd6ce1ba3cc8645b320ea41f7d7cfe8312
Extorial <christopherwang197@gmail.com>
2025-04-18 01:51:27 -0400

    Update to pull flowers straight from database
    Update index and views to pull flowers straight from django database admin. No more dummy data. Chapter 6 of Django BOok

--- flowerstore/flowers/views.py
+++ flowerstore/flowers/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
+from .models import Flower
 
 # Create your views here.
 flowers = [
-    {
-        'id': 1, 'name': 'Rose', 'price': 14.99, 'description': 'A beautiful red rose.'
-    },
-    {
-        'id': 2, 'name': 'Dandelion', 'price': 10.99, 'description': 'A petite dandelion.'
-    },
-    {
-        'id': 3, 'name': 'Tulip', 'price': 13.99, 'description': 'A gorgeous orange tulip.'
-    },
-    {
-        'id': 4, 'name': 'Sakura', 'price': 21.99, 'description': 'A smattering of pink sakura blossoms.'
-    },
+    
 ]
 
 def index(request):
-    template_data = {'title': 'Flowers', 'flowers': flowers}
+    template_data = {'title': 'Flowers', 'flowers': Flower.objects.all()}
     return render(request, 'flowers/index.html', {'template_data' : template_data})
 
 def detail(request, id):
