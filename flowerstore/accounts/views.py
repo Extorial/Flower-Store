9413f2ac4351d84c09ef212101452aa27ce78fde
Extorial <christopherwang197@gmail.com>
2025-04-26 23:10:26 -0400

    Update basic signup
    Update basic signup with custom sign up form, new error list, and started progress on login page. Chapter 8 of the Django book.

--- flowerstore/accounts/views.py
+++ flowerstore/accounts/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login, authenticate
+from django.shortcuts import redirect
+from .forms import CustomUserCreationForm, CustomErrorList
 # Create your views here.
+
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method =='GET':
+        return render(request, 'accounts/login.html', {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html', {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
 
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html', {'template_data': template_data})
+    elif request.method =='POST':
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
