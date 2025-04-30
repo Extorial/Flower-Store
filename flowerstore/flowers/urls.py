dc7ab4146bba3a2230d164501b3fc3a5708c2cff
Extorial <christopherwang197@gmail.com>
2025-04-30 02:18:37 -0400

    Implement reviews
    Implement reviews, creating, updating, deleting. Finished chapter 9 of Django book.

--- flowerstore/flowers/urls.py
+++ flowerstore/flowers/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='flowers.index'),
     path('<int:id>/', views.detail, name='flowers.detail'),
     path('<int:id>/review/create/', views.create_review, name='flowers.create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='flowers.edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='flowers.delete_review'),
 ]
--- flowerstore/flowers/views.py
+++ flowerstore/flowers/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Flower, Review
-from django.contrib.auth.models import login_required
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 flowers = [
@@ -18,7 +18,8 @@ def index(request):
 
 def detail(request, id):
     flower = Flower.objects.get(id=id)
-    template_data = {'title': flower.name, 'flower': flower}
+    reviews = Review.objects.filter(flower=flower)
+    template_data = {'title': flower.name, 'flower': flower, 'reviews': reviews}
     return render(request, 'flowers/detail.html', {'template_data' : template_data})
 
 @login_required
@@ -33,3 +34,25 @@ def create_review(request, id):
         return redirect('flowers.detail', id=id)
     else:
         return redirect('flowers.detail', id=id)
+    
+@login_required
+def edit_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        return redirect('flowers.detail', id=id)
+    if request.method == 'GET':
+        template_data = {'title': 'Edit Review', 'review': review}
+        return render(request, 'flowers/edit_review.html', {'template_data': template_data})
+    elif request.method == 'POST' and request.POST['comment'] != '':
+        review = Review.objects.get(id=review_id)
+        review.comment = request.POST['comment']
+        review.save()
+        return redirect('flowers.detail', id=id)
+    else:
+        return redirect('flowers.detail', id=id)
+    
+@login_required
+def delete_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id, user=request.user)
+    review.delete()
+    return redirect('flowers.detail', id=id)
